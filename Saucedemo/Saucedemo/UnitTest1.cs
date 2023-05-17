dd18c00b18f6c06246b35fdfcee87842cc3fb454
Darya Prasniak <125681242+DaryaPrasniak@users.noreply.github.com>
2023-05-17 18:20:10 +0200

    init

---
+++ Saucedemo/Saucedemo/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Saucedemo
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
